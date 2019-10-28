136c83a0c97141e5675c131745ceed40e8a5523f
IAmBlind <50620870+IAmBlind@users.noreply.github.com>
2019-10-28 21:46:10 +0100

    Initial Commit

---
+++ src/Interface.java
@@ -0,0 +1,124 @@
+import java.util.Map;
+import java.util.HashMap;
+
+public class Interface implements url {
+    // Variable
+    private String url;
+
+    public static void main(String[] args) {
+
+    }
+
+    // Testing Constructor
+    public Interface(String s) {
+        this.url = s;
+    }
+
+    @Override
+    public String getRawUrl() {
+        return url;
+    }
+
+    @Override
+    public String getPath() {
+        //Variable
+        int x = url.indexOf("/");
+        int y = url.indexOf("//");
+        int z = url.indexOf("?");
+
+        if(x < 0){      // check if x in Array
+            return "";  // no path available
+        }else if(y > 0 && z > 0){
+            String[] m_parameter = url.split("\\?");            // cut parameter
+            String[] m_https = m_parameter[0].split("//");      // cut "//"
+            String[] m_path = m_https[1].split("/", 2);    // cut domain
+            return "/" + m_path[1];                                     // return the path
+        }else {
+            return url;
+        }
+    }
+
+    @Override
+    public Map<String, String> getParameter() {
+        Map<String, String> _parameter = new HashMap<>();
+        String parameters = url.substring(url.lastIndexOf("?") + 1);    // get parameter
+        if(parameters.length() > 1){
+            for(String para : parameters.split("&")){
+                String[] pair = para.split("=");                      // identifier
+                String key = pair[0];
+                String value = "";
+                if(pair.length > 1) value = pair[1];
+                _parameter.put(key,value);                                   // input to hashmap
+            }
+        }
+        return _parameter;
+    }
+
+    @Override
+    public int getParameterCount() {
+        int p_count;
+        String[] _url = url.split("\\?");                               // search for ?
+        if(_url.length >= 2){
+            String [] parameter = _url[1].split("=");
+            p_count = parameter.length - 1;
+            return p_count;
+        }else {
+            return 0;
+        }
+    }
+
+    @Override
+    public String[] getSegments() {
+        //String[] part = url.split("/");
+        //String url = part[part.length -1];
+        String[] _segments = url.split("/");                // get segments of url
+        return _segments;                                          // return the segments
+    }
+
+    @Override
+    public String getFileName() {
+        //Variable
+        int i;
+        String file;
+        String[] x;
+        String _extension = url.substring(url.lastIndexOf('/'));    //get file
+        i = _extension.indexOf(".");
+        if(i <= 0){
+            return "";      //return nothing
+        }else {
+            if(_extension.contains("?")){
+                file = _extension.substring(0, _extension.indexOf("?"));    // cut segments
+                x = file.split("/");
+                return x[1];                // return file name
+            }
+            x = _extension.split("/");
+            return x[1];                    // return file name
+        }
+    }
+
+    @Override
+    public String getExtension() {
+        String temp = url.substring(url.lastIndexOf('/'));
+        int i = temp.indexOf(".");                                          // identifier
+        if(i > 0){
+            String _extension = url.substring(url.lastIndexOf('.'));    // cut the file name
+            if(_extension.contains("?")){
+                return _extension.substring(0, _extension.indexOf('?'));
+            }
+            return _extension;                                              // return extension
+        } else {
+            return "";                                                      // return nothing
+        }
+    }
+
+    @Override
+    public String getFragment(){
+        int i = url.indexOf("#");           // search #
+        if(i > 0){
+            String[] fragment = url.split("#");
+            return fragment[1];             //return fragment
+        }else {
+            return "";                      // return nothing
+        }
+    }
+}
---
+++ src/InterfaceTest.java
@@ -0,0 +1,150 @@
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+// Function testing
+public class InterfaceTest {
+
+    // Test of getRawUrl
+    @Test
+    public void t_rawUrl(){
+        Interface object = new Interface("www.testing.at/home/user/file.txt");
+        String out = object.getRawUrl();
+        Assertions.assertEquals("www.testing.at/home/user/file.txt", out);
+    }
+    @Test
+    public void t_rawUrl2(){
+        Interface object = new Interface("https://testing.at/home/user/file.txt?x=6&y=9&z=42#xx");
+        String out = object.getRawUrl();
+        Assertions.assertEquals("https://testing.at/home/user/file.txt?x=6&y=9&z=42#xx", out);
+    }
+
+    // Test of getPath
+    /*@Test
+    public void t_Path(){
+        Interface object = new Interface("www.testing.at/home/user/file.txt");
+        String out = object.getPath();
+        Assertions.assertEquals("/home/user/file.txt", out);
+    }*/
+
+    @Test
+    public void t_Path(){
+        Interface object = new Interface("https://testing.at/home/user/file.txt?x=6&y=9&z=42#xx");
+        String out = object.getPath();
+        Assertions.assertEquals("/home/user/file.txt", out);
+    }
+
+    /*@Test
+    public void t_Path3(){
+        Interface object = new Interface("");
+        String out = object.getPath();
+        Assertions.assertEquals("", out);
+    }*/
+
+
+    // Test of getParameterCount
+    @Test
+    public void t_PCount(){
+        Interface object = new Interface("www.testing.at/home/user/file?x=6&y=9&z=42#xx");
+        int out = object.getParameterCount();
+        Assertions.assertEquals(3, out);
+    }
+
+    @Test
+    public void t_PCount2(){
+        Interface object = new Interface("www.testing.at/home/user/file");
+        int out = object.getParameterCount();
+        Assertions.assertEquals(0, out);
+    }
+
+    /*@Test
+    public void t_PCount3(){
+        Interface object = new Interface("");
+        int out = object.getParameterCount();
+        Assertions.assertEquals("", out);
+    }*/
+
+    // Test of getParameter
+    @Test
+    public void t_Parameter(){
+        Interface object = new Interface("www.testing.at/home/user/file?x=6&y=9&z=42");
+        String out = object.getParameter().get("x");
+        String out2 = object.getParameter().get("y");
+        String out3 = object.getParameter().get("z");
+
+        Assertions.assertEquals("6", out);
+        Assertions.assertEquals("9", out2);
+        Assertions.assertEquals("42", out3);
+    }
+
+    // Test of getFileName
+    @Test
+    public void t_FileName(){
+        Interface object = new Interface("https://testing.at/home/user/file.txt");
+        String out = object.getFileName();
+        Assertions.assertEquals("file.txt", out);
+    }
+
+    @Test
+    public void t_FileName2(){
+        Interface object = new Interface("www.testing.at/home/user/file.txt");
+        String out = object.getFileName();
+        Assertions.assertEquals("file.txt", out);
+    }
+
+    @Test
+    public void t_FileName3(){
+        Interface object = new Interface("www.testing.at/home/user/");
+        String out = object.getFileName();
+        Assertions.assertEquals("", out);
+    }
+
+    // Test of getExtension
+    @Test
+    public void t_Extension(){
+        Interface object = new Interface("www.testing.at/home/user/file?x=6&y=9&z=42");
+        String out = object.getExtension();
+        Assertions.assertEquals("", out);
+    }
+
+    @Test
+    public void t_Extension2(){
+        Interface object = new Interface("https://testing.at/home/user/file");
+        String out = object.getExtension();
+        Assertions.assertEquals("", out);
+    }
+
+    @Test
+    public void t_Extension3(){
+        Interface object = new Interface("https://testing.at/home/user/file.txt");
+        String out = object.getExtension();
+        Assertions.assertEquals(".txt", out);
+    }
+
+    // Test of getFragment
+    @Test
+    public void t_Fragment(){
+        Interface object = new Interface("https://testing.at/home/user/file?x=6&y=9&z=42#xx");
+        String out = object.getFragment();
+        Assertions.assertEquals("xx", out);
+    }
+
+    @Test
+    public void t_Fragment2(){
+        Interface object = new Interface("https://testing.at/home/user/file");
+        String out = object.getFragment();
+        Assertions.assertEquals("", out);
+    }
+
+    // Test of getSegment
+    /*@Test
+    public void t_Segment(){
+        Interface object = new Interface("");
+        String[] out = object.getSegments();
+        Assertions.assertEquals("", out);
+    }*/
+}
+
+// Request Tests
+
+
+// Response Tests
